df442d80778aa9a10eac94bb5bd6e5089c7eef39
thgr8ganzi <q33asq33as@gmail.com>
2025-05-02 01:46:29 +0900

    feat: add Python DAG template with example tasks

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -15,19 +15,19 @@ with DAG (
         print(f"start_date: {start_date}")
         print(f"end_date: {end_date}")
 
-        python_t1 = PythonOperator(
-            task_id="python_t1",
-            python_callable=python_function,
-            op_kwargs={'start_date': '{{ data_interval_start | ds }}', 'end_date': '{{ data_interval_end | ds }}'},
-        )
+    python_t1 = PythonOperator(
+        task_id="python_t1",
+        python_callable=python_function,
+        op_kwargs={'start_date': '{{ data_interval_start | ds }}', 'end_date': '{{ data_interval_end | ds }}'},
+    )
 
-        @task(task_id="python_t2")
-        def python_function2(**kwargs):
-            print(kwargs)
-            print('ds: ' + str(kwargs['ds']))
-            print('ts: ' + str(kwargs['ts']))
-            print('data_interval_start: ' + str(kwargs['data_interval_start']))
-            print('data_interval_end: ' + str(kwargs['data_interval_end']))
-            print('task_instance: ' + str(kwargs['ti']))
+    @task(task_id="python_t2")
+    def python_function2(**kwargs):
+        print(kwargs)
+        print('ds: ' + str(kwargs['ds']))
+        print('ts: ' + str(kwargs['ts']))
+        print('data_interval_start: ' + str(kwargs['data_interval_start']))
+        print('data_interval_end: ' + str(kwargs['data_interval_end']))
+        print('task_instance: ' + str(kwargs['ti']))
 
     python_t1 >> python_function2()
