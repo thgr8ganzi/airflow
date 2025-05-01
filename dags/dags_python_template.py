287fb95443c5b33e5f57c9dea217fa604be5b6f1
thgr8ganzi <q33asq33as@gmail.com>
2025-05-02 01:50:27 +0900

    feat: add Python DAG template with example tasks

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -1,19 +1,27 @@
 from airflow import DAG
 import pendulum
-import datetime
-from airflow.decorators import task
 from airflow.operators.python import PythonOperator
+from airflow.decorators import task
 
-with DAG (
+def python_function(start_date, end_date):
+    print(f"start_date: {start_date}")
+    print(f"end_date: {end_date}")
+
+@task(task_id="python_t2")
+def python_function2(**kwargs):
+    print(kwargs)
+    print('ds: ' + str(kwargs['ds']))
+    print('ts: ' + str(kwargs['ts']))
+    print('data_interval_start: ' + str(kwargs['data_interval_start']))
+    print('data_interval_end: ' + str(kwargs['data_interval_end']))
+    print('task_instance: ' + str(kwargs['ti']))
+
+with DAG(
     dag_id="dags_python_template",
     schedule="30 9 * * * *",
     start_date=pendulum.datetime(2023, 10, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
-
-    def python_function(start_date, end_date):
-        print(f"start_date: {start_date}")
-        print(f"end_date: {end_date}")
 
     python_t1 = PythonOperator(
         task_id="python_t1",
@@ -21,13 +29,7 @@ with DAG (
         op_kwargs={'start_date': '{{ data_interval_start | ds }}', 'end_date': '{{ data_interval_end | ds }}'},
     )
 
-    @task(task_id="python_t2")
-    def python_function2(**kwargs):
-        print(kwargs)
-        print('ds: ' + str(kwargs['ds']))
-        print('ts: ' + str(kwargs['ts']))
-        print('data_interval_start: ' + str(kwargs['data_interval_start']))
-        print('data_interval_end: ' + str(kwargs['data_interval_end']))
-        print('task_instance: ' + str(kwargs['ti']))
+    # 데코레이터 태스크는 함수 호출로 인스턴스 생성
+    python_t2 = python_function2()
 
-    python_t1 >> python_function2()
+    python_t1 >> python_t2
