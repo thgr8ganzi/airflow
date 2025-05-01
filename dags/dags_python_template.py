885e4a06b28ab38a7e6353b78ee4d766311b332c
thgr8ganzi <q33asq33as@gmail.com>
2025-05-02 01:54:53 +0900

    feat: add Python DAG template with example tasks

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -1,27 +1,18 @@
 from airflow import DAG
 import pendulum
-from airflow.operators.python import PythonOperator
 from airflow.decorators import task
+from airflow.operators.python import PythonOperator
 
-def python_function(start_date, end_date):
-    print(f"start_date: {start_date}")
-    print(f"end_date: {end_date}")
-
-@task(task_id="python_t2")
-def python_function2(**kwargs):
-    print(kwargs)
-    print('ds: ' + str(kwargs['ds']))
-    print('ts: ' + str(kwargs['ts']))
-    print('data_interval_start: ' + str(kwargs['data_interval_start']))
-    print('data_interval_end: ' + str(kwargs['data_interval_end']))
-    print('task_instance: ' + str(kwargs['ti']))
-
-with DAG(
+with DAG (
     dag_id="dags_python_template",
     schedule="30 9 * * * *",
     start_date=pendulum.datetime(2023, 10, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
+
+    def python_function(start_date, end_date):
+        print(f"start_date: {start_date}")
+        print(f"end_date: {end_date}")
 
     python_t1 = PythonOperator(
         task_id="python_t1",
@@ -29,6 +20,13 @@ with DAG(
         op_kwargs={'start_date': '{{ data_interval_start | ds }}', 'end_date': '{{ data_interval_end | ds }}'},
     )
 
-    python_t2 = python_function2()
+    @task(task_id="python_t2")
+    def python_function2(**kwargs):
+        print(kwargs)
+        print('ds: ' + str(kwargs['ds']))
+        print('ts: ' + str(kwargs['ts']))
+        print('data_interval_start: ' + str(kwargs['data_interval_start']))
+        print('data_interval_end: ' + str(kwargs['data_interval_end']))
+        print('task_instance: ' + str(kwargs['ti']))
 
-    python_t1 >> python_t2
+    python_t1 >> python_function2()
