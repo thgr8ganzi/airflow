df7a7a30f01b2b9516aa8a14ec76c8d3c4b48d8d
thgr8ganzi <q33asq33as@gmail.com>
2025-05-01 16:03:08 +0900

    feat: 커넥션 테스트용 DAG 업로드

---
+++ dags/dags_cron_test.py
@@ -0,0 +1,47 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.eempty import EmptyOperator
+
+with DAG (
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * * *",
+    start_date=pendulum.datetime(2023, 10, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+
+    t1 = EmptyOperator(
+        task_id="t1",
+    )
+
+    t2 = EmptyOperator(
+        task_id="t2",
+    )
+
+    t3 = EmptyOperator(
+        task_id="t3",
+    )
+
+    t4 = EmptyOperator(
+        task_id="t4",
+    )
+
+    t5 = EmptyOperator(
+        task_id="t5",
+    )
+
+    t6 = EmptyOperator(
+        task_id="t6",
+    )
+
+    t7 = EmptyOperator(
+        task_id="t7",
+    )
+
+    t8 = EmptyOperator(
+        task_id="t8",
+    )
+
+    t1 >> [t2, t3] >> t4
+    t5 >> t4
+    [t4, t7] >> t6 >> t8
