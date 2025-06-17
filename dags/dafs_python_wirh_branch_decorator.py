6e27a4581cd3fff5c7db48917c7dcc683f78dd6d
thgr8ganzi <q33asq33as@gmail.com>
2025-06-18 00:22:18 +0900

    feat: add Python DAG task.branch

---
+++ dags/dafs_python_wirh_branch_decorator.py
@@ -0,0 +1,45 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.decorators import task
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    dag_id="dags_python_with_branch_decorator",
+    start_date=datetime(2023, 10, 1, tzinfo=pendulum.timezone("UTC")),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    @task.branch(task_id="python_branch_task")
+    def select_random():
+        import random
+        item_lst = ['A', 'B', 'C']
+        selected_random = random.choice(item_lst)
+        if selected_random == 'A':
+            return 'task_a'
+        elif selected_random in ['B', 'C']:
+            return ['task_b', 'task_c']
+
+    def common_func(**kwargs):
+        print(kwargs['selected'])
+
+    task_a = PythonOperator(
+        task_id="task_a",
+        python_callable=common_func,
+        op_kwargs={'selected': 'A'},
+    )
+
+    task_b = PythonOperator(
+        task_id="task_b",
+        python_callable=common_func,
+        op_kwargs={'selected': 'B'},
+    )
+
+    task_c = PythonOperator(
+        task_id="task_c",
+        python_callable=common_func,
+        op_kwargs={'selected': 'C'},
+    )
+
+    select_random() >> [task_a, task_b, task_c]
