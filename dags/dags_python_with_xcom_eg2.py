0b41802fd4a884bcee270c8c64e5b68630fceb22
thgr8ganzi <q33asq33as@gmail.com>
2025-06-17 21:46:59 +0900

    fix: add Python DAG with xcom variable

--- dags/dags_python_with_xcom_eg2.py
+++ dags/dags_python_with_xcom_eg2.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 
 with DAG (
     dag_id="dags_python_with_xcom_eg2",
-    schedule="30 6 * * * *",
+    schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 10, 1, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
@@ -22,11 +22,10 @@ with DAG (
         print(f"메소드 인자값: {result}")
 
     @task(task_id="python_xcom_pull_2")
-    def  xcom_pull_2(**kwargs):
+    def xcom_pull_2(**kwargs):
         ti = kwargs['ti']
         result = ti.xcom_pull(task_ids='python_xcom_push_by_return')
         print(f"함수 입력값: {result}")
 
     python_xcom_push_by_return = xcom_push_result()
-    xcom_pull_2(python_xcom_push_by_return)
-    python_xcom_push_by_return >> xcom_pull_1()
+    python_xcom_push_by_return >> [xcom_pull_1(), xcom_pull_2()]
